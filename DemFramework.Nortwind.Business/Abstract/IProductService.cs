3b53ee520dbf708f3e6fe38bbe149284a4c0dacf
fatihdemirtc <fatihdemirtc@gmail.com>
2018-06-18 23:50:58 +0300

    Validate

---
+++ DemFramework.Nortwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemFramework.Nortwind.Entities.Concrete;
+
+namespace DemFramework.Nortwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+        Product Update(Product product);
+    }
+}
---
+++ DemFramework.Nortwind.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemFramework.Core.CrossCuttingConcerns.Validation.FluentValidation;
+using DemFramework.Nortwind.Business.Abstract;
+using DemFramework.Nortwind.Business.ValidationRules.FluentValidation;
+using DemFramework.Nortwind.DataAccess.Abstract;
+using DemFramework.Nortwind.Entities.Concrete;
+
+namespace DemFramework.Nortwind.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        [FluentValidate(typeof(ProductValidatior))]
+        public Product Add(Product product)
+        {
+            ValidatorTool.FluentValidate(new ProductValidatior(), product);
+            return _productDal.Add(product);
+        }
+
+        [FluentValidate(typeof(ProductValidatior))]
+        public Product Update(Product product)
+        {
+            ValidatorTool.FluentValidate(new ProductValidatior(), product);
+            return _productDal.Update(product);
+        }
+    }
+}
